28922399ae110806006ace2d508e71f00c6c7443
Tao Jinlin <jinlin.tao@hexagonsi.com>
2020-02-03 16:11:26 +0800

    优化部分docker文件

--- eureka-service/docker/run.sh
+++
@@ -1,2 +0,0 @@
-java $JAVA_OPTS -jar /eureka-service-0.0.1-SNAPSHOT.jar \
-    --eureka.host=$EUREKA_HOST --server.port=$SERVER_PORT
--- event-service/docker/run.sh
+++
@@ -1,3 +0,0 @@
-java $JAVA_OPTS -jar /event-service-0.0.1-SNAPSHOT.jar \
-    --eureka.host=$EUREKA_HOST --server.host=$SERVER_HOST --server.port=$SERVER_PORT  \
-    --mysql.connectstring=$MYSQL_CONNECTSTRING --mysql.user=$MYSQL_USER --mysql.password=$MYSQL_PASSWORD
